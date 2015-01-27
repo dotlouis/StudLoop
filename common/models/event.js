1283328afcbc1ad99806883a01f2e5b55019dfeb
dotlouis <1891109+dotlouis@users.noreply.github.com>
2015-01-27 14:44:28 +0100

    Format an ical event into a proper Event instance
    - Course is matched and added to the Event
    - TODO: add contributors

--- common/models/event.js
+++ common/models/event.js
@@ -1,3 +1,30 @@
+var Promise = require('bluebird');
+
 module.exports = function(Event) {
 
+    Event.on('dataSourceAttached', function( obj ){
+        // wrap the whole model in Promise but avoid the 'validate' method
+        Event = Promise.promisifyAll( Event, {filter: function(name, func, target){
+            return !( name == 'validate');
+        }} );
+    });
+
+    Event.format = function(rawEvent){
+        var Course = Event.app.models.Course;
+
+        var course = Course.matchFromString(rawEvent.summary);
+        // var contributors = CustomUser.matchFromString(rawEvent.description);
+
+        return course.then(function(course){
+            return new Event({
+                start: rawEvent.start,
+                end: rawEvent.end,
+                location: rawEvent.location,
+                course: course
+                // type: course.type
+                // attendees: contributors.attendees,
+                // speakers: contributors.speakers
+            });
+        });
+    };
 };
